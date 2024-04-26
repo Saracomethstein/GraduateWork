8f2a077ef9ec230bb39fcc5a5dddd6e1edffd3b5
barathea <barathea@student.21-school.ru>
2024-04-26 14:39:58 +0300

    add registration

--- Connection/ServiceConnection.cs
+++ Connection/ServiceConnection.cs
@@ -10,13 +10,25 @@ namespace Connection
 {
     public class ServiceConnection : IServiceConnection
     {
-        public const string filePath = @"C:\ProgramData\cursorposition.db";
+        public const string filePath = @"C:\ProgramData\TestDataBase.db";
 
         public static SQLiteConnection connection = new SQLiteConnection($"Data Source = {filePath}; Version = 3; New = True; Compress = True;");
 
         public void AddNewUserInDB(string username, string password)
         {
-            throw new NotImplementedException();
+            SQLiteCommand cmd = connection.CreateCommand();
+            connection.Open();
+
+            cmd.CommandText = "INSERT OR IGNORE INTO users" +
+                "(user_name, user_password)" +
+                "VALUES" +
+                "(@username, @password);";
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            connection.Close();
         }
 
         public bool CheckUser(string username, string password)
@@ -38,7 +50,18 @@ namespace Connection
 
         public bool CheckUserExists(string username)
         {
-            throw new NotImplementedException();
+            SQLiteCommand cmd = connection.CreateCommand();
+            connection.Open();
+
+            cmd.CommandText = "SELECT * FROM users WHERE user_name = @username";
+            cmd.Parameters.AddWithValue("@username", username);
+
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+            cmd.ExecuteNonQuery();
+            connection.Close();
+
+            return count > 0;
         }
     }
 }
